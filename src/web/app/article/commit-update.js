4a6da291008434acac5180db469a758cb1c45994
zjh <zhanjh@126.com>
2018-10-08 15:15:35 +0800

    CtrlPanel

--- src/web/app/article/commit-update.js
+++ src/web/app/article/commit-update.js
@@ -1,7 +1,17 @@
 import {MonacoEditor} from 'MonacoEditor';
+import {CtrlPanel} from 'CtrlPanel';
 import {oneElem} from 'web-util';
 
 export default async core => {
+    const pageElem = oneElem('.page');
     const editor = new MonacoEditor(core.setting.pageConfig.commitDto.content);
-    editor.appendTo(oneElem('.page'));
+    const ctrlPanel = new CtrlPanel({
+        menuItems: [
+            {title: 'item1'},
+            {title: 'item2'}
+        ]
+    });
+
+    editor.appendTo(pageElem);
+    ctrlPanel.appendTo(pageElem);
 };
