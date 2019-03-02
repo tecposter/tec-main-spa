5169507a410ffbe530d5a572c4e65031ed1228aa
zhanjh <zhanjh@126.com>
2019-03-02 18:49:58 +0800

    Export pdf

--- src/web/app/article/markdown.js
+++ src/web/app/article/markdown.js
@@ -48,6 +48,33 @@ export default async () => {
         releasedContent = await articleCtrl.asFetchReleasedContent(commit.slug);
         return releasedContent;
     };
+
+    ctrlPanel.register(
+        {key: 'pdf', desc: 'PDF'},
+        () => {
+            const mywindow = window.open('', 'PRINT', 'height=650,width=900,top=100,left=150');
+            const title = 'test';
+            const divId = 'editor-preview';
+
+            mywindow.document.write(`<html><head><title>${title}</title>`);
+            mywindow.document.write(`
+                <link rel="stylesheet" href="//static.tecposter.cn/ui/dev/css/main.css">
+                <link type="text/css" rel="stylesheet" media="screen,print" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css">
+                <link type="text/css" rel="stylesheet" media="screen,print" href="https://cdn.jsdelivr.net/npm/katex@0.10.0/dist/katex.min.css" integrity="sha384-9eLZqc9ds8eNjO3TmqPeYcDj8n+Qfa4nuSiGYa6DjLNcv9BtN69ZIulL9+8CqC9Y" crossorigin="anonymous">
+            `);
+            mywindow.document.write('</head><body >');
+            mywindow.document.write(document.getElementById(divId).innerHTML);
+            mywindow.document.write('</body></html>');
+
+            mywindow.document.close(); // necessary for IE >= 10
+            mywindow.focus(); // necessary for IE >= 10*/
+
+            mywindow.print();
+            //mywindow.close();
+
+            return true;
+        }
+    );
 };
 
 const gotoShowArticle = (slug) => {
